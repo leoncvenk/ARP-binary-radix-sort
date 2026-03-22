63e35d2de722885301a407b9a8a3173acfa7eb28
leoncvenk <leon.cvenk2@gmail.com>
2026-03-22 22:20:16 +0100

    Dodana osnova glavnega programa

--- vaja2.cpp
+++ vaja2.cpp
@@ -108,7 +108,13 @@ bool zapisiPodatke(const char* imeDatoteke, unsigned char* A, int n)
     return true;
 }
 
-int main() {
-    cout << "Test" << endl;
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        cout << "Uporaba: vaja2 <vhodna_datoteka>" << endl;
+        return 1;
+    }
+
     return 0;
 }
